05cbaae5f53c46b1257586f5abe59ff2be72b83e
Martin Mwenda <43547404+MartinMugambi@users.noreply.github.com>
2023-01-22 19:48:08 +0300

    adding api fetched product data

--- src/components/productsRowOne/productsCard.tsx
+++ src/components/productsRowOne/productsCard.tsx
@@ -1,36 +1,38 @@
 import { prototype } from "events";
 import "./productsCard.css";
 import { ReactNode } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 type ProductsCardProps = {
-  productType: string;
-  image: string;
-  productName: string;
-  icon: ReactNode;
+  title: string;
+  price: string;
   description: string;
-  cost: string;
+  category: string;
+  image: string;
 };
-
 export const ProductsCard = ({
-  productType,
-  image,
-  productName,
-  icon,
+  title,
+  price,
   description,
-  cost,
+  category,
+  image,
 }: ProductsCardProps) => {
+  const newDescription = description.slice(0, 52);
+  const newTitle = title.slice(0, 18);
+
   return (
     <>
       <section className="products-card">
-        <p className="product-type">{productType}</p>
+        <p className="product-type">{category}</p>
         <div className="info">
           <div className="image">
             <img src={image} alt="" />
           </div>
-          <h5>{productName}</h5>
-          <div className="rating-icons">{icon}</div>
-          <p>{description}</p>
-          <h4>{cost}</h4>
+          <h5>{newTitle}</h5>
+          <div className="rating-icons">{}</div>
+          <p>{newDescription}...</p>
+          <h4>${price}</h4>
           <button>Add to Basket</button>
         </div>
       </section>
--- src/components/productsRowTwo/productsTwo.tsx
+++ src/components/productsRowTwo/productsTwo.tsx
@@ -1,6 +1,8 @@
 import "./productsTwo.css";
 import { GrStar } from "react-icons/gr";
 import { ProductsCard } from "../productsRowOne/productsCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const image1 = require("./jewellery.jpeg");
 const image2 = require("./electronics.jpg");
@@ -8,148 +10,28 @@ const image3 = require("./women.jpg");
 const image4 = require("./computing.png");
 
 export const ProductsTwoComponent = () => {
-  const products = [
-    {
-      productType: `jewelly`,
-      image: image1,
-      productName: "White Gold Plat",
-      icon: [<GrStar />, <GrStar />],
-      description: "Classic Created Wedding Engagement Solitaire Diamond...",
-      cost: "$67.89",
-    },
-    {
-      productType: `jewelly`,
-      image: image1,
-      productName: "White Gold Plat",
-      icon: [<GrStar />, <GrStar />],
-      description: "Classic Created Wedding Engagement Solitaire Diamond...",
-      cost: "$67.89",
-    },
-    {
-      productType: `jewelly`,
-      image: image1,
-      productName: "White Gold Plat",
-      icon: [<GrStar />, <GrStar />],
-      description: "Classic Created Wedding Engagement Solitaire Diamond...",
-      cost: "$67.89",
-    },
-    {
-      productType: `jewelly`,
-      image: image1,
-      productName: "White Gold Plat",
-      icon: [<GrStar />, <GrStar />],
-      description: "Classic Created Wedding Engagement Solitaire Diamond...",
-      cost: "$67.89",
-    },
-    {
-      productType: `electronics`,
-      image: image2,
-      productName: "WD 4TB Gamming D",
-      icon: [<GrStar />, <GrStar />],
-      description: "Expand Your PS4 Gamming Experience, Play anywhere fast...",
-      cost: "$67.89",
-    },
-    {
-      productType: `electronics`,
-      image: image2,
-      productName: "WD 4TB Gamming D",
-      icon: [<GrStar />, <GrStar />],
-      description: "Expand Your PS4 Gamming Experience, Play anywhere fast...",
-      cost: "$67.89",
-    },
-    {
-      productType: `electronics`,
-      image: image2,
-      productName: "WD 4TB Gamming D",
-      icon: [<GrStar />, <GrStar />],
-      description: "Expand Your PS4 Gamming Experience, Play anywhere fast...",
-      cost: "$67.89",
-    },
-    {
-      productType: `electronics`,
-      image: image2,
-      productName: "WD 4TB Gamming D",
-      icon: [<GrStar />, <GrStar />],
-      description: "Expand Your PS4 Gamming Experience, Play anywhere fast...",
-      cost: "$67.89",
-    },
-    {
-      productType: `women's clothing`,
-      image: image3,
-      productName: "MBJ Women's Sol",
-      icon: [<GrStar />, <GrStar />],
-      description: "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not…",
-      cost: "$9.95",
-    },
-    {
-      productType: `women's clothing`,
-      image: image3,
-      productName: "MBJ Women's Sol",
-      icon: [<GrStar />, <GrStar />],
-      description: "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not…",
-      cost: "$9.95",
-    },
-    {
-      productType: `women's clothing`,
-      image: image3,
-      productName: "MBJ Women's Sol",
-      icon: [<GrStar />, <GrStar />],
-      description: "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not…",
-      cost: "$9.95",
-    },
-    {
-      productType: `women's clothing`,
-      image: image3,
-      productName: "MBJ Women's Sol",
-      icon: [<GrStar />, <GrStar />],
-      description: "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not…",
-      cost: "$9.95",
-    },
-    {
-      productType: `computing`,
-      image: image4,
-      productName: "Laptop Lacunari",
-      icon: [<GrStar />, <GrStar />],
-      description:
-        "Core i5, 64 Bits Operating System, 6.2 Inches, 1TB Harddisk, Touchscreen...",
-      cost: "$9.95",
-    },
-    {
-      productType: `computing`,
-      image: image4,
-      productName: "Laptop Lacunari",
-      icon: [<GrStar />, <GrStar />],
-      description:
-        "Core i5, 64 Bits Operating System, 6.2 Inches, 1TB Harddisk, Touchscreen...",
-      cost: "$9.95",
-    },
-    {
-      productType: `computing`,
-      image: image4,
-      productName: "Laptop Lacunari",
-      icon: [<GrStar />, <GrStar />],
-      description:
-        "Core i5, 64 Bits Operating System, 6.2 Inches, 1TB Harddisk, Touchscreen...",
-      cost: "$9.95",
-    },
-    {
-      productType: `computing`,
-      image: image4,
-      productName: "Laptop Lacunari",
-      icon: [<GrStar />, <GrStar />],
-      description:
-        "Core i5, 64 Bits Operating System, 6.2 Inches, 1TB Harddisk, Touchscreen...",
-      cost: "$9.95",
-    },
-  ];
+  const [apiProducts, setApiProducts] = useState<any[]>([]);
 
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products").then((res) => {
+      console.log(res.data);
+      setApiProducts(res.data);
+    });
+  }, []);
+
+  let slicedApiProducts = apiProducts.slice(4, 20);
   return (
     <>
       <section className="products-two">
+        {slicedApiProducts.map((product) => {
+          return <ProductsCard key={product.id} {...product} />;
+        })}
+      </section>
+      {/* <section className="products-two">
         {products.map((product, index) => {
           return <ProductsCard key={index} {...product} />;
         })}
-      </section>
+      </section> */}
     </>
   );
 };
